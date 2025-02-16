3fb35d8d2a2ba616eb92b46b4dbebc230342fa26
leobaehre <leobaehre@gmail.com>
2025-02-15 23:09:41 -0600

    Initial Commit

---
+++ Homeworky/HomeworkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkyApp.swift
+//  Homeworky
+//
+//  Created by Leo Bähre on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeworkyTests/HomeworkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkyTests.swift
+//  HomeworkyTests
+//
+//  Created by Leo Bähre on 2/15/25.
+//
+
+import Testing
+@testable import Homeworky
+
+struct HomeworkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
